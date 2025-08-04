173333886457b6dab5f72d7e9b085b23f081de14
Haydar Mehryar <haydarmehryar@gmail.com>
2025-08-04 17:44:00 -0400

    sim Add training scripts for MambaOut variants: none, in12k, and in12k_ft_in1k initializations @hmehryar resolves #4

---
+++ experiments/exp_250723_init_strategy_mambaout_mambavision/train_mambaout_base_plus_rw_g500_31824713.py
@@ -0,0 +1,276 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import os
+import torch
+import torch.nn as nn
+
+
+from config import get_config
+from data.loader import get_dataloaders
+from models.model import create_model
+from train_utils.resume import init_resume_state
+from train_utils.resume import fill_trackers_from_history
+from train_utils.train_epoch import train_one_epoch
+from train_utils.evaluate import evaluate
+from train_utils.train_metrics_logger import update_train_logs
+from train_utils.train_metrics_logger import update_val_logs
+from train_utils.checkpoint_saver import save_epoch_checkpoint
+from train_utils.train_metrics_logger import record_and_save_epoch
+from train_utils.scheduler_utils import create_scheduler
+from train_utils.early_stopping import check_early_stopping
+from train_utils.training_summary import finalize_training_summary
+from train_utils.training_summary import print_best_model_summary
+from train_utils.plot_metrics import plot_train_val_metrics
+from train_utils.plot_metrics import plot_loss_accuracy
+
+
+# In[ ]:
+
+
+# cfg=get_config(config_path="config/convnext_fb_in22k_ft_in1k_bs512_ep50_lr1e-04_ds1000.yml")
+# cfg=get_config(config_path="config/convnext_fb_in1k_bs512_ep50_lr1e-04_ds1000.yml")
+# cfg=get_config(config_path="config/convnext_gaussian_bs512_ep50_lr1e-04_ds1000.yml")
+# cfg=get_config(config_path="config/efficientnet_bs512_ep50_lr1e-01_ds1000_sched-RLRP.yml")
+# cfg=get_config(config_path="config/vit_bs512_ep50_lr1e-04_ds1000.yml")
+# cfg=get_config(config_path="config/mambaout_base_plus_rw_bs32_ep50_lr1e-04_ds1000-g1.yml")
+cfg=get_config()
+print(cfg)
+
+
+# In[3]:
+
+
+os.makedirs(cfg.output_dir, exist_ok=True)
+print(f"[INFO] Saving all outputs to: {cfg.output_dir}")
+
+
+# In[4]:
+
+
+# Set seed, device
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"[INFO] Using device: {device}")
+# torch.backends.cudnn.benchmark = True
+
+
+# In[5]:
+
+
+# Data
+train_loader, val_loader, test_loader = get_dataloaders(cfg)
+
+
+# In[6]:
+
+
+# Model and optimizer
+model, optimizer = create_model(cfg.backbone, cfg.input_shape, cfg.learning_rate)
+model.to(device)
+
+
+# In[7]:
+
+
+if torch.cuda.device_count() > 1:
+    print(f"Using {torch.cuda.device_count()} GPUs")
+    model = torch.nn.DataParallel(model)
+
+
+# In[8]:
+
+
+scheduler = create_scheduler(optimizer, cfg, train_loader=train_loader)
+
+
+# In[9]:
+
+
+if torch.cuda.device_count() > 1:
+    print(f"Using {torch.cuda.device_count()} GPUs")
+    model = torch.nn.DataParallel(model)
+else:
+    print("Using single GPU")
+    
+
+
+# In[10]:
+
+
+criterion = {
+    # 'energy_loss_output': nn.BCELoss(),
+    'energy_loss_output': nn.BCEWithLogitsLoss(),
+    'alpha_output': nn.CrossEntropyLoss(),
+    'q0_output': nn.CrossEntropyLoss()
+}
+print(f"[INFO] Loss functions:{criterion}")
+
+
+# In[11]:
+
+
+print(f"[INFO] Init Training Trackers")
+train_loss_energy_list, train_loss_alpha_list, train_loss_q0_list, train_loss_list = [], [], [],[]
+train_acc_energy_list, train_acc_alpha_list, train_acc_q0_list, train_acc_list = [], [], [], []
+
+print(f"[INFO] Init Validation Trackers")
+val_loss_energy_list, val_loss_alpha_list,val_loss_q0_list,val_loss_list = [], [], [], []
+val_acc_energy_list, val_acc_alpha_list,val_acc_q0_list ,val_acc_list = [],[],[],[]
+
+
+# In[12]:
+
+
+model, optimizer, start_epoch, best_acc, early_stop_counter, best_epoch, best_metrics, training_summary, all_epoch_metrics,summary_status = init_resume_state( model, optimizer, device,cfg)
+
+
+# In[13]:
+
+
+fill_trackers_from_history(
+    all_epoch_metrics,
+    train_loss_energy_list, train_loss_alpha_list, train_loss_q0_list, train_loss_list,
+    train_acc_energy_list, train_acc_alpha_list, train_acc_q0_list, train_acc_list,
+    val_loss_energy_list, val_loss_alpha_list, val_loss_q0_list, val_loss_list,
+    val_acc_energy_list, val_acc_alpha_list, val_acc_q0_list, val_acc_list,
+    summary_status, best_epoch
+)
+
+
+# In[14]:
+
+
+# for testing
+# train_metrics = train_one_epoch(train_loader, model, criterion, optimizer, device)
+# print(f"[INFO] Training metrics: {train_metrics}")
+
+
+# In[15]:
+
+
+for epoch in range(start_epoch, cfg.epochs):
+    print(f"[INFO] Epoch {epoch+1}/{cfg.epochs}")
+    train_metrics={}
+    train_metrics = train_one_epoch(train_loader, model, criterion, optimizer, device)
+    (train_loss_list,
+    train_loss_energy_list,
+    train_loss_alpha_list,
+    train_loss_q0_list,
+    train_acc_list,
+    train_acc_energy_list,
+    train_acc_alpha_list,
+    train_acc_q0_list
+    ) = update_train_logs(
+        train_metrics,
+        train_loss_list,
+        train_loss_energy_list,
+        train_loss_alpha_list,
+        train_loss_q0_list,
+        train_acc_list,
+        train_acc_energy_list,
+        train_acc_alpha_list,
+        train_acc_q0_list
+    )
+    val_metrics = evaluate(val_loader, model, criterion, device)
+    (val_loss_list,
+    val_loss_energy_list,
+    val_loss_alpha_list,
+    val_loss_q0_list,
+    val_acc_list,
+    val_acc_energy_list,
+    val_acc_alpha_list,
+    val_acc_q0_list,
+    ) = update_val_logs(
+        val_metrics,
+        val_loss_list,
+        val_loss_energy_list,
+        val_loss_alpha_list,
+        val_loss_q0_list,
+        val_acc_list,
+        val_acc_energy_list,
+        val_acc_alpha_list,
+        val_acc_q0_list,
+    )
+    print(f"[INFO] Epoch {epoch+1}: Energy Acc ={val_metrics['energy']['accuracy']:.4f}, αs Acc = {val_metrics['alpha']['accuracy']:.4f}, Q0 Acc = {val_metrics['q0']['accuracy']:.4f}, Total Acc = {val_metrics['accuracy']:.4f}")
+    print(f"[INFO] Epoch {epoch+1}: Energy Loss ={val_metrics['loss_energy']:.4f}, αs Loss = {val_metrics['loss_alpha']:.4f}, Q0 Loss = {val_metrics['loss_q0']:.4f}, Total Loss = {val_metrics['loss']:.4f}")
+    
+    scheduler.step(val_metrics['accuracy'])  # or macro average accuracy if defined
+    for param_group in optimizer.param_groups:
+        current_lr = param_group['lr']
+        print(f"📉 Current LR: {current_lr}")
+    
+    all_epoch_metrics=record_and_save_epoch(epoch, train_metrics, val_metrics, current_lr, all_epoch_metrics, cfg.output_dir)
+    
+    # save_epoch_checkpoint(
+    #     epoch=epoch,
+    #     model=model,
+    #     optimizer=optimizer,
+    #     metrics=val_metrics,
+    #     output_dir=cfg.output_dir
+    # )
+
+    best_acc, best_metrics, best_epoch, early_stop_counter, should_stop = check_early_stopping(
+        best_acc=best_acc,
+        best_metrics=best_metrics,
+        early_stop_counter=early_stop_counter,
+        best_epoch=best_epoch,
+        model=model,
+        optimizer=optimizer,
+        val_metrics=val_metrics,
+        output_dir=cfg.output_dir,
+        patience=cfg.patience,
+        epoch=epoch
+    )
+    
+    if should_stop:
+        break
+    
+    print("="*150)
+    
+
+
+# In[ ]:
+
+
+finalize_training_summary(
+    summary=training_summary,
+    best_epoch=best_epoch,
+    best_acc=best_acc,
+    best_metrics=best_metrics,
+    output_dir=cfg.output_dir
+)
+print_best_model_summary(
+    best_epoch=best_epoch,
+    best_acc=best_acc,
+    best_metrics=best_metrics
+)
+
+
+# In[ ]:
+
+
+plot_train_val_metrics(train_loss_list, val_loss_list, train_acc_list, val_acc_list, cfg.output_dir)
+plot_loss_accuracy(train_loss_list,
+                    train_loss_energy_list,
+                    train_loss_alpha_list,
+                    train_loss_q0_list,
+                    train_acc_list,
+                    train_acc_energy_list,
+                    train_acc_alpha_list,
+                    train_acc_q0_list,
+                    cfg.output_dir,
+                    title="Train Loss and Accuracy per Epoch")
+plot_loss_accuracy(val_loss_list,
+                    val_loss_energy_list,
+                    val_loss_alpha_list,
+                    val_loss_q0_list,
+                    val_acc_list,
+                    val_acc_energy_list,
+                    val_acc_alpha_list,
+                    val_acc_q0_list,
+                    cfg.output_dir,
+                    title="Validation Loss and Accuracy per Epoch")
+
